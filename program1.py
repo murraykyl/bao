196f3aa6f6d01eec5f80b0128301ab9908638b80
Kyle Murray <kmurr10@ur.rochester.edu>
2015-06-03 15:38:25 -0400

    First Histogram

--- program1.py
+++ program1.py
@@ -4,11 +4,17 @@ import numpy as np
 from astropy.io import fits
 from astropy.table import Table
 import matplotlib.pyplot as plt
+import ROOT as r
 
-
-data = fits.getdata('Downloads/galaxies_DR9_CMASS_North.fits', 1)
+r.gROOT.SetBatch(1)
+data = fits.getdata('../Downloads/galaxies_DR9_CMASS_North.fits', 1)
 t = Table(data)
 print(t[0]['ra'])
 
+h = r.TH1D("hist", "", 100, 0, 1000)
 for row in t:
-    print(row['ra'])
+   h.Fill(row['ra'])
+c = r.TCanvas()
+h.Draw()
+c.Print("output.pdf")
+
