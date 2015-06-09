47a85658b74aa0721c90df350d50404eb33181d1
Kyle Murray <kmurr10@ur.rochester.edu>
2015-06-09 17:09:28 -0400

    Correlation Functions Defined

--- correlation.py
+++ correlation.py
@@ -1,16 +1,5 @@
-from astropy.io import fits
-from astropy.table import table
-import numpy as np
-import ROOT as r
 import math
 from scipy import integrate
-
-#Set up Histogram
-r.gROOT.setBATCH(1)
-
-#Access the table we will use
-data = getdata('../Downloads/galaxies_DR9_CMASS_North.fits', 1)
-t = table(data)
 
 #Define Constants
 c = 3e5     #km/s
@@ -24,45 +13,35 @@ omega = {"K0": 0,  #Curvature
 
 #Define row arguments
 for row in t:
-   ra = row['ra']
-   dec = row['dec']
-   z1 = row['z']
+    x,y,z = cartesian(row)
 
-#Define Primary Functions
 def hubbleRatio(z):                                         #H(z)/H_0
-    return sqrt( omega["K0"] * (1+z)**2 +
-                 omega["M0"] * (1+z)**3 +
-                 omega["R0"] * (1+z)**4 +
-                 omega["L0"])
+    return sqrt(omega["K0"] * (1+z)**2 +
+                omega["M0"] * (1+z)**3 +
+                omega["R0"] * (1+z)**4 +
+                omega["L0"])
 
-def distanceMeasure(z):                                     #Distance Measure
+def distanceMeasure(z):                                     #Comoving Distance Measure
     return (integrate.quad(lambda:x, 1/hubbleRatio(x), 
                            0, z) / H0) * c
 
-#Define New Variable and Convert to Cartesian Coordinates
-r = distanceMeasure(z1)                #Comoving Distances
-ra1 = math.degrees(ra)                 #Convert to Degrees
-dec1 = math.degrees(dec)               #Convert to Degrees
-x = r * math.cos(ra1) * math.cos(dec1) #X-Coordinate
-y = r * math.sin(ra1) * math.cos(dec1) #Y-Coordinate
-z = r * math.sin(dec1)                 #Z-Coordinate
+def cartesian(row):                                         #Convert to Cartesian Coordinates
+    """Define New Variable and Convert to Cartesian Coordinates"""
+    r = distanceMeasure(row['z'])               #Comoving Distances
+    ra1 = math.pi*row['ra']/180                 #Convert to Radians
+    dec1 = math.pi*row['dec']/180               #Convert to Radians
+    x = r * math.cos(ra1) * math.cos(dec1)      #X-Coordinate
+    y = r * math.sin(ra1) * math.cos(dec1)      #Y-Coordinate
+    z = r * math.sin(dec1)                      #Z-Coordinate
+    return x,y,z
 
-#Define Distances between Galaxies
-for i in range(len(t)):
-    xd = x - x[i:]
-    yd = y - y[i:]
-    zd = z - z[i:]
-dist = sqrt(xd**2 + yd**2 + zd**2)     #THIS IS OUR NEW VARIABLE FOR THE CORRELATION FUNCTION
+def absoluteDistance(xd,yd,zd):                             #Define Distances between Galaxies
+    return sqrt(xd**2 + yd**2 + zd**2)
 
-#Create Correlation Function Method 1
-def correlationDataData(i):
-    for i in range(len(t)):
-        return dist - dist[i:]
-
-#Create Histogram for New Function
-corr = r.TH1D("DD(re)", "Data-Data;Distance;Counts per Bin", 100, dataData(zlo), dataData(zhi))
-for row in t:
-    corr.Fill(dataData(dist))
-c = r.Tcanvas()
-corr/Draw()
-c.Print("Data-Data.pdf")
+def loopOverPairs(tab):                                     #Create Correlation Function
+    for i in range(len(tab)):
+        for j in range(len(tab))[i+1:]:
+            xi,yi,zi = cartesian(tab[i])
+            xj,yj,zj = cartesian(tab[j])
+            d = absoluteDistance(xi-xj, yi-yj, zi-zj)
+            print d
