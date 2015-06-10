d88e0664f7d3e1dce77c0fe760e81d51d6138ccf
Kyle Murray <kmurr10@ur.rochester.edu>
2015-06-10 15:55:19 -0400

    Working Version

--- correlation.py
+++ correlation.py
@@ -1,7 +1,7 @@
 import math
 from scipy import integrate
+import ROOT as r
 
-#Define Constants
 c = 3e5     #km/s
 h = 0.7     #dimensionless universe scale
 H0 = 100*h  #km/s/Mpc
@@ -11,21 +11,17 @@ omega = {"K0": 0,  #Curvature
          "R0": 0,  #Radiation
          "L0": .7} #Cosmological Constant
 
-#Define row arguments
-for row in t:
-    x,y,z = cartesian(row)
-
-def hubbleRatio(z):                                         #H(z)/H_0
-    return sqrt(omega["K0"] * (1+z)**2 +
+def hubbleRatio(z):                             #H(z)/H_0
+    return math.sqrt(omega["K0"] * (1+z)**2 +
                 omega["M0"] * (1+z)**3 +
                 omega["R0"] * (1+z)**4 +
                 omega["L0"])
 
-def distanceMeasure(z):                                     #Comoving Distance Measure
-    return (integrate.quad(lambda:x, 1/hubbleRatio(x), 
-                           0, z) / H0) * c
+def distanceMeasure(z):                         #Comoving Distance Measure
+    return (integrate.quad(lambda m: 1/hubbleRatio(m), 
+                           0, z)[0] / H0) * c
 
-def cartesian(row):                                         #Convert to Cartesian Coordinates
+def cartesian(row):                             #Convert to Cartesian Coordinates
     """Define New Variable and Convert to Cartesian Coordinates"""
     r = distanceMeasure(row['z'])               #Comoving Distances
     ra1 = math.pi*row['ra']/180                 #Convert to Radians
@@ -35,13 +31,22 @@ def cartesian(row):                                         #Convert to Cartesia
     z = r * math.sin(dec1)                      #Z-Coordinate
     return x,y,z
 
-def absoluteDistance(xd,yd,zd):                             #Define Distances between Galaxies
-    return sqrt(xd**2 + yd**2 + zd**2)
+def absoluteDistance(xd,yd,zd):                 #Define Distances between Galaxies
+    return math.sqrt(xd**2 + yd**2 + zd**2)
 
-def loopOverPairs(tab):                                     #Create Correlation Function
+def histAA(tab):                                #Create Correlation Function
     for i in range(len(tab)):
         for j in range(len(tab))[i+1:]:
             xi,yi,zi = cartesian(tab[i])
             xj,yj,zj = cartesian(tab[j])
             d = absoluteDistance(xi-xj, yi-yj, zi-zj)
-            print d
+            histAA = r.TH1D("Galaxy Pairs", "", 100, 0, 1000)
+            histAA.Fill(d)
+        if i == 2:
+            break
+
+if __name__=="__main__":                        #Test Functions
+    print hubbleRatio(.5)
+    print distanceMeasure(.6)
+    print absoluteDistance(12-7, 7-3, 9+17)
+    
